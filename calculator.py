0804635bd8e8cd00243c7e4ebf5b0f2c962d87bb
A <lloyd.a.higgins@gmail.com>
2024-05-30 21:29:44 -0700

    added error exceptions

--- calculator.py
+++ calculator.py
@@ -12,11 +12,13 @@ txt_path2 = Path("operation_display.txt")
 operation_display = txt_path2.read_text()
 
 def input_select():
+
     operation_select = int
     active = True
     while active:
-        print(operation_display)
+
         try:
+            print(operation_display)
             operation_select = int(input(">: "))
         except ValueError:
             print("Error, unknown value")
@@ -24,53 +26,59 @@ def input_select():
         if operation_select == 1:
             addition()
 
-        elif operation_select == 2:
-            subtraction()
 
-        elif operation_select == 3:
-            multiply()
-
-        elif operation_select == 4:
-            divide()
-
-        elif operation_select == 5:
-            sqrt()
-
-        elif operation_select == 7:
-            active = False
-
-# add comments for everything/descriptions
-# this is a test
+# functions for two integers
+# addition
 def addition():
-    number1 = int(input("Please enter the first number: "))
-    number2 = int(input("Please enter the second number: "))
-    print(":", number1 + number2)
 
-def subtraction():
-    number1 = int(input("Please enter the first number: "))
-    number2 = int(input("Please enter the second number: "))
-    print(":", number1 - number2)
+    while True:
 
+        try:
+            num1 = int(input("Enter the first number: "))
+            num2 = int(input("Enter the second number: "))
+            break
+        except ValueError:
+            print("Error: unknown value")
+
+    print(":", num1 + num2)
+
+
+# subtract
+def subtract():
+    while True:
+        try:
+            num1 = int(input("Enter the first number: "))
+            num2 = int(input("Enter the second number: "))
+            break
+        except ValueError:
+            print("Error: unknown value")
+    print(":", num1 - num2)
+
+"""
+# multiply
 def multiply():
     number1 = int(input("Please enter the first number: "))
     number2 = int(input("Please enter the second number: "))
     print(":", number1 * number2)
 
+# divide
 def divide():
     number1 = int(input("Please enter the first number: "))
     number2 = int(input("Please enter the second number: "))
     print(":", number1 / number2)
 
+# sqrt
 def sqrt():
     number1 = int(input("Enter a number to find the square root: "))
     print(":", math.sqrt(number1))
 
+# even or odd comparison
 def evenOdd():
     number1 = int(input("Enter a number to find if it is even or odd: "))
     if number1 % 2 == 0:
         print(f"{number1} is even.")
     else:
         print(f"{number1} is odd")
-
+"""
 print(calc_display)
 input_select()
