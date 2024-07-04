1546b9f036003251aec9e6dc76937a40a697e2ab
A <lloyd.a.higgins@gmail.com>
2024-07-04 11:36:28 -0700

    Added multiple numbers to the addition function.

--- calculator.py
+++ calculator.py
@@ -30,12 +30,20 @@ def input_select():
             addition()
         elif operation_select == 2:
             subtract()
+        elif operation_select == 3:
+            multiply()
+        elif operation_select == 4:
+            divide()
+        elif operation_select == 5:
+            sqrt()
+        elif operation_select == 6:
+            even_odd()
         elif operation_select == 7:
             break
 
 # functions for adding multiple integers
 # 0 input = 0
-# addition
+# 1: addition
 def addition():
 
     num_list = []
@@ -58,7 +66,7 @@ def addition():
 
     print(sum(num_list))
 
-# subtract
+# 2: subtract
 def subtract():
     while True:
         try:
@@ -69,32 +77,30 @@ def subtract():
             print("Error: unknown value")
     print(":", num1 - num2)
 
-
-# multiply
+# 3: multiply
 def multiply():
     number1 = int(input("Choose a number to multiply: "))
     number2 = int(input("Enter number of times to multiply: "))
     print(":", number1 * number2)
 
-"""
-# divide
+# 4: divide
 def divide():
     number1 = int(input("Please enter the first number: "))
     number2 = int(input("Please enter the second number: "))
     print(":", number1 / number2)
 
-# sqrt
+# 5: sqrt
 def sqrt():
     number1 = int(input("Enter a number to find the square root: "))
     print(":", math.sqrt(number1))
 
-# even or odd comparison
-def evenOdd():
+# 6: even/odd comparison
+def even_odd():
     number1 = int(input("Enter a number to find if it is even or odd: "))
     if number1 % 2 == 0:
         print(f"{number1} is even.")
     else:
         print(f"{number1} is odd")
-"""
+
 print(calc_display)
 input_select()
