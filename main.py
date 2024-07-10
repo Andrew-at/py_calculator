514f935878e425abf3d10ac753e2620088413338
L <andrewlhiggs@proton.me>
2024-07-10 14:50:02 -0700

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ This is a side project using some
 beginner Python fundamentals (loops, error checking, ect.)
 to build a calculator that uses a terminal window.
 The ultimate goal is to recreate some math functions from the Python
-math library without using anything from the library itself.
+math library.
 """
 
 from pathlib import Path
